97a11f7780332372762dc1db48e3b84468d9fb1c
elvanov <elvanov@hotmail.com>
2020-04-23 10:23:06 +0200

    Code upgrade to have everything in functions

--- Run_MOGAMUN_Example1_Linux.R
+++ Run_MOGAMUN_Example1_Linux.R
@@ -5,5 +5,5 @@ setUpEvolutionProcessParameters(Generations=2)
 loadData(DEPath = "~/My_Experiment/Differential_Expression_Results.csv",
          NodesScoresPath = "~/My_Experiment/Genes_With_Nodes_Scores.csv",
          NetworkLayersDir = "~/Layers_Multiplex/",
-         LayersToUse <- "123")
+         Layers <- "123")
 mogamun(resultsDir = "~/My_Experiment/MOGAMUN_Results/")
--- Run_MOGAMUN_Example2_Windows.R
+++ Run_MOGAMUN_Example2_Windows.R
@@ -5,5 +5,5 @@ setUpEvolutionProcessParameters(Generations=3)
 loadData(DEPath = "C:/Users/User/Documents/My_Experiment/Differential_Expression_Results.csv",
          NodesScoresPath = "C:/Users/User/Documents/My_Experiment/Genes_With_Nodes_Scores.csv",
          NetworkLayersDir = "C:/Users/User/Documents/Layers_Multiplex/",
-         LayersToUse <- "123")
+         Layers <- "123")
 mogamun(resultsDir = "C:/Users/User/Documents/My_Experiment/MOGAMUN_Results/")
